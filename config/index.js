2a75fa5cb797747b0b06c5ccf1c400f2305e50b8
zhangjingjing <zhangjingjing2020@gmail.com>
2019-04-01 23:05:47 +0800

    新增微信登录

--- config/index.js
+++ config/index.js
@@ -4,6 +4,8 @@ const config = {
     host: env.HOST,
     port: env.PORT,
     jwtSecret: env.JWT_SECRET,
+    wxAppid: env.WX_APPID,
+    wxSecret: env.WX_SECRET,
 };
 
 module.exports = config;
--- routes/hello-hapi.js
+++ routes/hello-hapi.js
@@ -5,7 +5,6 @@ module.exports = [{
     method: 'GET',
     path: '/',
     handler: (request, reply) => {
-        console.log(request.auth.credentials);
         reply('hello hapi');
     },
     config: {
